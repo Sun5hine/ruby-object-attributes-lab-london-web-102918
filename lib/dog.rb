61b14abc328be432e01aeb3dd71fcdd30d1b0451
sun5hine <af5kh@yahoo.com>
2018-11-01 13:33:29 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name 
+    @name 
+    
+    def initalize(name, breed)
+      @name = name 
+      @breed = breed 
+    
+    def name=(new_name)
+      @name = new_name 
+      
+      def breed 
+        @breed 
+        
+        def breed=(new_breed)
+          @breed = new_breed 
+  
+end
